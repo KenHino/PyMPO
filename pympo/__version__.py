c0cc99dbb982a2e8065931ac675b26b519a0b6b0
Kentaro Hino <63284175+KenHino@users.noreply.github.com>
2024-11-26 19:38:37 +0900

    Update __version__.py

--- pympo/__version__.py
+++ pympo/__version__.py
@@ -1,5 +1,5 @@
 import importlib.metadata
 
-_DISTRIBUTION_METADATA = importlib.metadata.metadata("pompon")
+_DISTRIBUTION_METADATA = importlib.metadata.metadata("pympo")
 __version__ = _DISTRIBUTION_METADATA["Version"]
 __version_info__ = tuple(map(int, __version__.split(".")))
