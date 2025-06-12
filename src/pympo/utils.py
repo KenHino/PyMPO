b44503aef14186248024d00958c3adab1e5d1cc6
Kentaro Hino <chem2016@OX.AC.UK>
2025-06-12 11:06:16 +0100

    start util method project

---
+++ src/pympo/utils.py
@@ -0,0 +1,22 @@
+def qr(mpo):
+    raise NotImplementedError
+
+
+def svd(mpo, r: float):
+    raise NotImplementedError
+
+
+def export_npz(mpo, filename):
+    raise NotImplementedError
+
+
+def import_npz(filename):
+    raise NotImplementedError
+
+
+def export_itensor_hdf5(mpo, filename):
+    raise NotImplementedError
+
+
+def import_itensor_hdf5(filename):
+    raise NotImplementedError
