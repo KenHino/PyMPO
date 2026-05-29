1d79decc9145ee2624f36ae173dccf7d10c216c3
Charles Coulombe <ccoulombe@users.noreply.github.com>
2026-05-29 10:06:20 -0400

    Fix version info parsing to handle local version/tags

--- src/pympo/__version__.py
+++ src/pympo/__version__.py
@@ -2,4 +2,6 @@ import importlib.metadata
 
 _DISTRIBUTION_METADATA = importlib.metadata.metadata("pympo")
 __version__ = _DISTRIBUTION_METADATA["Version"]
-__version_info__ = tuple(map(int, __version__.split(".")))
+__version_info__ = tuple(
+    map(int, __version__.partition("+")[0].split("."))
+)
